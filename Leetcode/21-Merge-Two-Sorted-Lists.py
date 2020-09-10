3850a07057fad628ad4d6c6d59100674308051a4
safakozdek <safakozdek@gmail.com>
2020-09-11 01:26:56 +0300

    fix a solution

--- Leetcode/21-Merge-Two-Sorted-Lists.py
+++ Leetcode/21-Merge-Two-Sorted-Lists.py
@@ -5,23 +5,14 @@
 #         self.next = next
 class Solution:
     def mergeTwoLists(self, l1: ListNode, l2: ListNode) -> ListNode:
-        if not (l1 or l2):
-            return None
-        head = mergedList = ListNode()
+        head = cur = ListNode(0)
         while l1 and l2:
-            mergedList.val = min(l1.val, l2.val)
-            mergedList.next = ListNode()
-            mergedList = mergedList.next
             if l1.val < l2.val:
+                cur.next = l1
                 l1 = l1.next
             else:
+                cur.next = l2
                 l2 = l2.next
-
-        if l1:
-            mergedList.val = l1.val
-            mergedList.next = l1.next
-        if l2:
-            mergedList.val = l2.val
-            mergedList.next = l2.next
-
-        return head
+            cur = cur.next
+        cur.next = l1 or l2
+        return head.next
