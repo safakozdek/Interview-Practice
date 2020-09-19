a2c9d6cbfbcb7eae58a0389f856d6ff0d98b2007
safakozdek <safakozdek@gmail.com>
2020-09-19 13:55:24 +0300

    bfs solution

--- Leetcode/257-Binary-Tree-Paths.py
+++ Leetcode/257-Binary-Tree-Paths.py
@@ -5,17 +5,59 @@
 #         self.left = left
 #         self.right = right
 class Solution:
-    #DFS - iterative
+    # DFS - iterative
     def binaryTreePaths(self, root: TreeNode) -> List[str]:
-        if not root:
+        if not root:  # empty check
             return []
-        res, stack = [], [(root, "")]
+        result = []
+        stack = [(root, "")]  # store tuple -> (node, path)
+
         while stack:
-            node, ls = stack.pop()
-            if not node.left and not node.right:
-                res.append(ls+str(node.val))
+            node, path = stack.pop()
+            if not node.left and not node.right:  # if leaf
+                result.append(path + str(node.val))
+                continue
+            # Add children to stack
             if node.right:
-                stack.append((node.right, ls+str(node.val)+"->"))
+                stack.append((node.right, path + str(node.val) + "->"))
             if node.left:
-                stack.append((node.left, ls+str(node.val)+"->"))
-        return res
+                stack.append((node.left, path + str(node.val) + "->"))
+
+        return result
+
+        # BFS - iterative
+    def binaryTreePaths2(self, root: TreeNode) -> List[str]:
+        if not root:  # empty check
+            return []
+        result = []
+        queue = [(root, "")]  # store tuple -> (node, path)
+
+        while queue:
+            node, path = queue.pop(0)
+            if not node.left and not node.right:  # if leaf
+                result.append(path + str(node.val))
+                continue
+            # Add children to stack
+            if node.right:
+                queue.append((node.right, path + str(node.val) + "->"))
+            if node.left:
+                queue.append((node.left, path + str(node.val) + "->"))
+
+        return result
+
+
+"""
+1. Check empty case right away. 
+- Checking it later might cause nested ifs
+- A lot more readable
+
+2. Use a stack/queue of tuple -> (node, path)
+- Path is stored as string 
+- Only keeping track of nodes won't work, we need paths
+
+3. Modify paths before adding children
+- path += str(node.val) + "->"
+
+4. In leaf nodes add path to result.
+result.append(path + str(node.val))
+"""
