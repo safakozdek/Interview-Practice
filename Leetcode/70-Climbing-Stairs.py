dfade7276da5a5a580b5fd8d255f41944e79bea8
safakozdek <safakozdek@gmail.com>
2020-09-25 14:59:18 +0300

    dp

--- Leetcode/70-Climbing-Stairs.py
+++ Leetcode/70-Climbing-Stairs.py
@@ -15,3 +15,17 @@ class Solution:
             return cache[n]
 
         return climb(n)
+
+        # dp
+        def climbStairs2(self, n: int) -> int:
+            if n == 1:
+                return 1
+
+            dp = [None] * (n + 1)
+            dp[1] = 1
+            dp[2] = 2
+
+            for i in range(3, n + 1):
+                dp[i] = dp[i - 1] + dp[i - 2]
+
+            return dp[n]
