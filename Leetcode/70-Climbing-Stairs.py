04dfcf3b7bb9672788762f0fd2b0059fd04964e9
Şafak <safakozdek@gmail.com>
2020-10-02 01:42:30 +0300

    dp

--- Leetcode/70-Climbing-Stairs.py
+++ Leetcode/70-Climbing-Stairs.py
@@ -16,16 +16,16 @@ class Solution:
 
         return climb(n)
 
-        # dp
-        def climbStairs2(self, n: int) -> int:
-            if n == 1:
-                return 1
+    # dp
+    def climbStairs2(self, n: int) -> int:
+        if n == 1:
+            return 1
 
-            dp = [None] * (n + 1)
-            dp[1] = 1
-            dp[2] = 2
+        dp = [None] * (n + 1)
+        dp[1] = 1
+        dp[2] = 2
 
-            for i in range(3, n + 1):
-                dp[i] = dp[i - 1] + dp[i - 2]
+        for i in range(3, n + 1):
+            dp[i] = dp[i - 1] + dp[i - 2]
 
-            return dp[n]
+        return dp[n]
