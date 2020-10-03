46ba037cd5656df1c5a38fca9fd21b0391303d1c
safakozdek <safakozdek@gmail.com>
2020-10-03 22:42:48 +0300

    dfs+backtrack

---
+++ Leetcode/39-Combination-Sum.py
@@ -0,0 +1,16 @@
+class Solution:
+    def combinationSum(self, candidates, target):
+        res = []
+        candidates.sort()
+        self.dfs(candidates, target, 0, [], res)
+        return res
+
+    def dfs(self, nums, target, index, path, res):
+        if target < 0:
+            return
+        if target == 0:
+            res.append(path)
+            return
+        for i in range(index, len(nums)):
+            self.dfs(nums, target-nums[i], i, path+[nums[i]], res)
+
